f1edd6bd40054754500705d98923c44b490f825e
Tom Hopkins <tom@githop.com>
2014-11-13 14:37:42 -0700

    adds contributions post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
   resources :zipcodes, except: [:new, :edit]
   resources :politicians, except: [:new, :edit]
+  post 'politicians/:id/contributions' => 'politicians#contributions'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
